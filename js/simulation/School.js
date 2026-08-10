05c26aedbbc24734f25e2a6f03bab9ff8dbcc41e
wenchy <167422521+Catcher-007@users.noreply.github.com>
2026-08-10 15:21:43 +0800

    refactor: make school own fish lifecycle

--- js/simulation/School.js
+++ js/simulation/School.js
@@ -6,13 +6,20 @@ export class School {
 
   setLeader(leader) {
     this.leader = leader;
-    if (leader) leader.isLeader = true;
+    if (leader) {
+      leader.isLeader = true;
+      if (!this.fish.includes(leader)) this.fish.push(leader);
+    }
   }
 
-  update(dt, width, height) {
-    for (const fish of this.fish) {
-      fish.update(dt, width, height);
-    }
+  add(fish) {
+    if (!this.fish.includes(fish)) this.fish.push(fish);
+    return fish;
+  }
+
+  update(dt, speed, width, height) {
+    for (const fish of this.fish) fish.update(speed, dt);
+    for (const fish of this.fish) fish.edge();
   }
 
   getLeader() {
